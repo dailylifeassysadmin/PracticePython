50ed9e57cdbd5ce5bcb73ac73ae16b7552a1f911
dailylifeassysadmin <raviindia77@hotmail.com>
2025-09-19 23:31:58 +0530

    Added: This is a program for reversing a string input receive from the user.

---
+++ 15-Reverse-Word-Order.py
@@ -0,0 +1,31 @@
+# Reverse Word Order
+# strings
+# Exercise 15 (and Solution)
+# Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order.
+# For example, say I type the string:
+
+# My name is Michele
+# Then I would see the string:
+
+# Michele is name My
+# shown back to me.
+
+word = input("Enter a long string containing multiple words: ")
+result = word.split(" ")
+
+# print(result.reverse())
+
+new_result = []
+
+for i in range(1, len(result)+1):
+    new_result.append(result[-i])
+    # new_result = new_result[0:0:-1]
+
+# print("newresult:", new_result)
+new_result = " ".join(new_result)
+print(new_result)
+
+# new_result[0:]
+# print("Newdata", new_result[::-1])
+# result = result[-1]
+# print(result)
